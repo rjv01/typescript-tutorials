92457e55e34e7826ac9c2626354af8c3488d2c63
rjv01 <verma.rajshekhar1@gmail.com>
2025-11-14 18:28:39 +0530

    in op

--- basics03/src/myGenerics.ts
+++ basics03/src/myGenerics.ts
@@ -1,37 +1,56 @@
-const score:Array<number> = [];
-const names:Array<string> = [];
+// const score:Array<number> = [];
+// const names:Array<string> = [];
 
-function one(val :number | boolean) :number|boolean {
-    return val;
-}
+// function one(val :number | boolean) :number|boolean {
+//     return val;
+// }
 
-function two(val :any) :any {
-    return val;
-}
+// function two(val :any) :any {
+//     return val;
+// }
 
-function three<Type>(val: Type):Type {
-    return val;
-}
+// function three<Type>(val: Type):Type {
+//     return val;
+// }
 
-one(1);
-two("2");
-three(3);
-three("4");
+// one(1);
+// two("2");
+// three(3);
+// three("4");
 
-interface Bottle{
-    brand:string,
-    liter:number
-}
+// interface Bottle{
+//     brand:string,
+//     liter:number
+// }
 
-// to create a function tpye as bottle we need to use Generic Type or T for short
+// // to create a function tpye as bottle we need to use Generic Type or T for short
+// // function four<Bottle>(val: Bottle) :Bottle{
+// //     return val;
+// // }
+
+// // four("gym",1);
+
 // function four<Bottle>(val: Bottle) :Bottle{
 //     return val;
 // }
 
-// four("gym",1);
+// four(1);
 
-function four<Bottle>(val: Bottle) :Bottle{
-    return val;
+
+interface Users{
+    name:string,
+    email:string
 }
 
-four(1);
+interface isAdmin{
+    name:string,
+    email:string,
+    isAdmin:boolean
+}
+
+function isAdminAccount(account: Users | isAdmin){
+    //here "in" is operator which checks if isAdmin is variable of isAdmin or Users;
+    if("isAdmin" in account){
+        return account.isAdmin;
+    }
+}
