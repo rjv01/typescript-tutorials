f8504d0e59d9d3a33bd6039593721e032b3be1f6
rjv01 <verma.rajshekhar1@gmail.com>
2025-11-27 21:33:29 +0530

    learning useState type format1

--- miniProjects/typeScriptMiniProject/src/components/User.tsx
+++ miniProjects/typeScriptMiniProject/src/components/User.tsx
@@ -18,10 +18,12 @@ export default function User() {
     }
     return (
         <div className='min-h-screen flex flex-col justify-center items-center'>
-            <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <p>user name is: </p>
-            <p>user email is: </p>
+            <div className='flex gap-4 mb-4'>
+                <button className='border-2 border-primary p-4 text-xl rounded-lg hover:bg-primary/50 cursor-pointer duration-150' onClick={handleLogin}>Login</button>
+                <button className='border-2 border-primary p-4 text-xl rounded-lg hover:bg-primary/50 cursor-pointer duration-150' onClick={handleLogout}>Logout</button>
+            </div>
+            <p className='text-2xl '>User name is: <span className='text-yellow-400'>{user?.name}</span> </p>
+            <p className='text-2xl '>User email is: <span className='text-yellow-400'>{user?.email}</span> </p>
         </div>
     )
 }
