bc8aef278e26cbf60772d415a696d32cea2caa7b
rjv01 <verma.rajshekhar1@gmail.com>
2025-12-25 21:05:16 +0530

    adding responsive

--- miniProjects/typeScriptMiniProject/src/components/TestButton.tsx
+++ miniProjects/typeScriptMiniProject/src/components/TestButton.tsx
@@ -25,16 +25,16 @@ const TestButton: React.FC<MyButtonProps> = (props) =>{
     <div 
       className='min-h-screen flex justify-center items-center flex-col m-4 p-4'
     >
-      <div className='flex flex-col justify-center items-center border border-white rounded-lg p-6 m-6'>
+      <div className='flex flex-col justify-center items-center border border-red-500 bg-red-500/20 text-red-400 rounded-lg p-6 m-6'>
         <h1 
           className={`text-4xl text-primary ${values < 0 ? "text-red-600" : "text-green-600"} mb-4`}>
             {values}
         </h1>
       <div
-        className='flex'  
+        className='flex sm:flex-row flex-col'  
       >
             <button 
-            className='border border-white p-3 m-3 rounded-lg cursor-pointer hover:bg-primary hover:text-secondary text-xl duration-150'
+            className='border p-3 m-3 rounded-lg cursor-pointer hover:bg-red-800/40 hover:text-red-500 text-xl duration-150'
             // onClick={props.onclick}
             onClick={() => {
               if(props.text === "Increase"){
@@ -49,13 +49,13 @@ const TestButton: React.FC<MyButtonProps> = (props) =>{
             {props.text}
           </button>
           <button 
-            className='border border-white p-3 m-3 rounded-lg cursor-pointer hover:bg-primary hover:text-secondary text-xl duration-150'
+            className='border p-3 m-3 rounded-lg cursor-pointer hover:bg-red-800/40 hover:text-red-500 text-xl duration-150'
             onClick={()=>setValues(values-1)}
             >
             Decrease
           </button>
           <button 
-            className='border border-white p-3 m-3 rounded-lg cursor-pointer hover:bg-primary hover:text-secondary text-xl duration-150'
+            className='border p-3 m-3 rounded-lg cursor-pointer hover:bg-red-800/40 hover:text-red-500 text-xl duration-150'
             onClick={()=>setValues(0)}
             >
             Reset
--- miniProjects/typeScriptMiniProject/src/pages/Stopwatch.tsx
+++ miniProjects/typeScriptMiniProject/src/pages/Stopwatch.tsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 //react.fc means react functional component
 const Stopwatch: React.FC = () => {
+    const [time,setTime] = useState<number>(0);
+    const intervalRef = useRef<number|null>(null);
+
+    const startTimer = ()=>{
+        if(intervalRef.current !== null)
+            return ;
+
+        intervalRef.current = setInterval(()=>{
+            setTime(prev => prev+1);
+        },500);
+    }
+    const stopTimer = ()=>{
+        if(intervalRef.current !== null){
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    }
+    const resetTimer = ()=>{
+        stopTimer();
+        setTime(0);
+    }
     return (
         <div className="min-h-screen flex flex-col items-center m-4 p-4">
             <h1 className="text-red-600 text-2xl mb-6">StopWatch App</h1>
-            <div className="flex flex-col justify-center items-center border border-white m-4 p-4">
-                <div className="flex justify-center text-2xl m-4 p-3 text-background bg-foreground rounded-lg">
-                    <span>00</span>:<span>00</span>:<span>00</span>
+            <div className="flex flex-col justify-center items-center border border-yellow-500/20 text-yellow-500 bg-yellow-400/20 m-4 p-4 rounded-lg">
+                <div className="flex justify-center items-center text-2xl m-4 p-4 text-background bg-gray-200 rounded-full h-16 w-16">
+                    <span>{time}</span>
                 </div>
-                <div className="flex m-2 p-2 gap-4">
-                    <p 
-                        className="border border-white rounded-lg cursor-pointer hover:bg-primary hover:text-secondary p-6 text-xl duration-150"
+                <div className="flex m-2 p-2 gap-4 flex-col sm:flex-row">
+                    <p
+                        onClick={resetTimer}
+                        className="border hover:text-yellow-200 border-yellow-500 hover:bg-yellow-700/20 rounded-lg cursor-pointer p-6 text-xl duration-150"
                     >Reset</p>
-                    <p 
-                        className="border border-white rounded-lg cursor-pointer hover:bg-primary hover:text-secondary p-6 text-xl duration-150"
+                    <p
+                        onClick={startTimer}
+                        className="border hover:text-yellow-200 border-yellow-500 hover:bg-yellow-700/20 rounded-lg cursor-pointer p-6 text-xl duration-150"
                     >Start</p>
-                    <p 
-                        className="border border-white rounded-lg cursor-pointer hover:bg-primary hover:text-secondary p-6 text-xl duration-150"
+                    <p
+                        onClick={stopTimer}
+                        className="border hover:text-yellow-200 border-yellow-500 hover:bg-yellow-700/20 rounded-lg cursor-pointer p-6 text-xl duration-150"
                     >Pasue</p>
                 </div>
             </div>
-            <a href="/">Home Page</a>
+            {/* <a href="/">Home Page</a> */}
         </div>
     )
 }
