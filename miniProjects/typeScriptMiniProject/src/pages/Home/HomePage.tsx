7cd8625d45c273f56fe0b82a4c80438f63af7af2
rjv01 <verma.rajshekhar1@gmail.com>
2025-12-25 19:49:02 +0530

    adding responsive

--- miniProjects/typeScriptMiniProject/src/pages/Home/HomePage.tsx
+++ miniProjects/typeScriptMiniProject/src/pages/Home/HomePage.tsx
@@ -3,34 +3,34 @@ import React from 'react'
 const HomePage: React.FC = () => {
   return (
     <div className='min-h-screen flex flex-col items-center m-4 p-4'>
-      <h1 className='text-2xl text-red-500'>I, rjv01 welcomes you to my Small Project collection on</h1>
-      <h1 className='text-2xl text-red-500'>TypeScript with Shadcn UI component library</h1>
-      <div className='grid grid-cols-3 m-4 p-4'>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/stopwatch">StopWatch</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/testbutton">Counter</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/enteryourname">Enter Your Name</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/userstatus">User Profile</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/greet">Greet</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/person">Person</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/heros">Hero</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/status">Status</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/buttons">Buttons</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/inputs">Inputs</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/stylepropss">Style props</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/loggedin">LoggedIn</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/users">Users</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/box">Box</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/timer">Timer</a>
-        <a className='text-blue-400 hover:text-white hover:bg-linear-to-b from-red-500 via-purple-500 to-blue-500 w-full text-xl p-3 m-2 border duration-200' href="/cinema">Cinematography</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/texteditor">Text Editor</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' href="/cards">Cards</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' 
+      <h1 className='text-2xl text-red-500 font-mono'>I, rjv01 welcomes you to my Small Project collection on</h1>
+      <h1 className='text-2xl text-red-500 font-mono'>TypeScript with Shadcn UI component library</h1>
+      <div className='grid sm:grid-cols-3 m-4 p-4 sm:gap-8 gap-6'>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/stopwatch">StopWatch</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/testbutton">Counter</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/enteryourname">Enter Your Name</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/userstatus">User Profile</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/greet">Greet</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/person">Person</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/heros">Hero</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/status">Status</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/buttons">Buttons</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/inputs">Inputs</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/stylepropss">Style props</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/loggedin">LoggedIn</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/users">Users</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/box">Box</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/timer">Timer</a>
+        <a className='text-blue-400 hover:text-white hover:bg-linear-to-r from-yellow-500 via-orange-500 to-yellow-500 w-full text-xl duration-200 p-3 m-2 rounded-lg' href="/cinema">Cinematography</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/texteditor">Text Editor</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' href="/cards">Cards</a>
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' 
         href="/paginationpage">Pagination Page</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' 
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' 
         href="/usercards">User Cards</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' 
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' 
         href="/shadcncalendar">ShadCn Calendar</a>
-        <a className='text-blue-400 hover:text-blue-900 text-xl p-3 m-2 border' 
+        <a className='text-blue-400 border-blue-500 bg-blue-500/20 text-center hover:text-blue-700 hover:bg-blue-500/30 text-xl duration-200 p-3 m-2 border rounded-lg' 
         href="/responsivepage">Responsive Pager</a>
       </div>
     </div>
